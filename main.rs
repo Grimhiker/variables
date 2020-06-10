de1e7412e8a5912b2c3d4c2d671c619f71c0ff21
Grimhiker <66091618+Grimhiker@users.noreply.github.com>
2020-06-10 10:08:30 -0500

    Add files via upload

---
+++ main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let tup = (500, 6.4, 1);
+
+    let (x, y, z) = tup;
+
+        println!("the value of x is: {}", x);
+
+        println!("the value of z is: {}", z);
+        
+        println!("the value of y is: {}", y);
+  
+}
